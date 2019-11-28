43cd8a275c00e0e291b9e0468203fedba8c9a5a1
Eric Hendrix <agentmrbig@gmail.com>
2019-11-28 00:28:22 -0500

    Todo list test app with react native, replaced scrollView with FlatList

--- App.js
+++ App.js
@@ -1,25 +1,71 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  }
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => {
+      return [...currentGoals,
+      { key: Math.random().toString(), value: enteredGoal }
+      ]
+    });
+  }
 
   return (
-    <View style={{ padding: 30 }}>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
         <TextInput placeholder="Course Goal"
-          style={{ width: '80%', borderBottomColor: 'black', borderWidth: 1, padding: 10 }} />
-        <Button title="ADD" />
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="ADD" style={styles.addButton} onPress={addGoalHandler} />
       </View>
+      <FlatList
+        data={courseGoals}
+        renderItem={itemData => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item.value}</Text>
+          </View>
+        )} />
+      {courseGoals.map((goal, index) => { return })}
+
 
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50,
+
   },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input: {
+    width: '80%',
+    borderBottomColor: 'black',
+    borderWidth: 1,
+    padding: 10
+  },
+  addButton: {
+    padding: 10,
+    height: '100%'
+  },
+  listItem: {
+    padding: 10,
+    marginTop: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
+  }
 });
